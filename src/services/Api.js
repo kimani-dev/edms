f97092b4a0c486e4b581f6745443a9b93e5437a4
David Kimani <daviemim07@gmail.com>
2022-09-29 18:31:16 +0300

    included live db url

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const apiClient = new axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "https://eparliamentapi.suvence.com/",
   credentials: true,
   headers: {
     Accept: "application/json",
@@ -11,7 +11,7 @@ const apiClient = new axios.create({
 });
 
 const fileClient = new axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "https://eparliamentapi.suvence.com/",
   credentials: true,
   headers: {
     "Content-Type": "multipart/form-data",
